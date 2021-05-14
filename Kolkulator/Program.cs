89e389233e33784c1e802796109d2771f842e36f
KoleckOLP-work <o.kurz@atlas.cz>
2021-05-14 12:01:25 +0200

    Add spaces before comment

--- Kolkulator/Program.cs
+++ Kolkulator/Program.cs
@@ -28,12 +28,12 @@ namespace Kolkulator
                           "5. Power\n" +
                           "6. Root\n" +
                           "7. Quadratic Equasion\n" +
-                          /*"8. Algebraic Expressions\n" +*/
+                          // "8. Algebraic Expressions\n" +
                           "8. Real HDD/Flash space\n" +
                           "9. Quit");
                 Write("#");
                 cho = ReadKey().KeyChar;
-                if(cho == '1') //Addition
+                if(cho == '1') // Addition
                 {
                     Clear();
                     WriteLine("type the first number and confirm it by <Enter>");
@@ -47,7 +47,7 @@ namespace Kolkulator
                     WriteLine("the result of {0} + {1} = {2}", num1, num2, res1);
                     shrt.Paktc();
                 }
-                else if(cho == '2') //Subtraction
+                else if(cho == '2') // Subtraction
                 {
                     Clear();
                     WriteLine("type the first number and confirm it by <Enter>");
@@ -61,7 +61,7 @@ namespace Kolkulator
                     WriteLine("the result of {0} - {1} = {2}", num1, num2, res1);
                     shrt.Paktc();
                 }
-                else if (cho == '3') //Multiplication
+                else if (cho == '3') // Multiplication
                 {
                     Clear();
                     WriteLine("type the first number and confirm it by <Enter>");
@@ -75,7 +75,7 @@ namespace Kolkulator
                     WriteLine("the result of {0} * {1} = {2}", num1, num2, res1);
                     shrt.Paktc();
                 }
-                else if (cho == '4') //Division
+                else if (cho == '4') // Division
                 {
                     Clear();
                     WriteLine("type the first number and confirm it by <Enter>");
@@ -89,7 +89,7 @@ namespace Kolkulator
                     WriteLine("the result of {0} / {1} = {2}", num1, num2, res1);
                     shrt.Paktc();
                 }
-                else if (cho == '5') //Power
+                else if (cho == '5') // Power
                 {
                     Clear();
                     WriteLine("type the number and confirm it with <Enter>");
@@ -102,7 +102,7 @@ namespace Kolkulator
                     WriteLine("the result of {0} to the power of {1} = {2}", num1, num2, res1);
                     shrt.Paktc();
                 }
-                else if (cho == '6') //Root
+                else if (cho == '6') // Root
                 {
                     Clear();
                     WriteLine("type the number and confirm it with <Enter>");
@@ -135,20 +135,20 @@ namespace Kolkulator
                     }
                     shrt.Paktc();
                 }
-                else if(cho == '7') //Quadratic Equasion
+                else if(cho == '7') // Quadratic Equasion
                 {
                     Clear();
                     WriteLine("type A in the equasion and confirm it with <Enter>");
                     Write("#");
-                    num1 = Double.Parse(ReadLine());//a
+                    num1 = Double.Parse(ReadLine());// a
                     WriteLine("type B in the equasion and confirm it with <Enter>");
                     Write("#");
-                    num2 = Double.Parse(ReadLine());//b
+                    num2 = Double.Parse(ReadLine());// b
                     WriteLine("type C in the equasion and confirm it with <Enter>");
                     Write("#");
-                    num3 = Double.Parse(ReadLine());//c
-                    res1 = (-num2 + Sqrt(num2 * num2 - 4 * num1 - num3)) / (2 * num1);//x1
-                    res2 = (-num2 - Sqrt(num2 * num2 - 4 * num1 - num3)) / (2 * num1);//x2
+                    num3 = Double.Parse(ReadLine());// c
+                    res1 = (-num2 + Sqrt(num2 * num2 - 4 * num1 - num3)) / (2 * num1);// x1
+                    res2 = (-num2 - Sqrt(num2 * num2 - 4 * num1 - num3)) / (2 * num1);// x2
                     Clear();
                     WriteLine(String.Format("the result of your Equasion is\n" +
                                             "x1 = {0:0.00}\n" +
@@ -160,13 +160,13 @@ namespace Kolkulator
                     Clear();
                     WriteLine("type size in GB and confirm it with <Enter>");
                     Write("#");
-                    num1 = Double.Parse(ReadLine());//a
+                    num1 = Double.Parse(ReadLine());// a
                     res1 = num1 * 1000.0 * 1000.0 * 1000.0 / 1024.0 / 1024.0 / 1024.0;
                     Clear();
                     WriteLine(String.Format("manufacturers say {0}GB but it's {1:0.00}", num1, res1));
                     shrt.Paktc();
                 }
-                else if (cho == '9') //Quit
+                else if (cho == '9') // Quit
                 {
                     break;
                 }
